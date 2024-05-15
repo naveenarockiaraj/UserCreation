6d018809bc509e14077d85272dfae6c7aec26354
naveenarockiaraj <naveenarockiaraj.paulraj@gmail.com>
2024-05-15 13:23:54 +0530

    DD changes

--- src/App.js
+++ src/App.js
@@ -4,22 +4,81 @@ import { useEffect, useState } from 'react';
 import { apiServices } from './apiServices';
 
 
-const cityData = {
-  Zhejiang: ['Hangzhou', 'Ningbo', 'Wenzhou'],
-  Jiangsu: ['Nanjing', 'Suzhou', 'Zhenjiang'],
-};
-const stateData = ['Zhejiang', 'Jiangsu'];
+
+// const countryData = ['India', 'USA', 'UK', 'Australia', 'Canada'];
+// const stateData = {
+//   India:['Kerala','Tamilnadu','Andhara','Karnataka'],
+//   USA:['Indiana','Lasvegas','Sanfransisco','NYC'],
+//   UK:['London','Manchester','Birmingham','Liverpool'],
+//   Australia:['Sydney','Melbourne','Brisbane','Perth'],
+//   Canada:['Toronto','Vancouver','Montreal','Calgary'],
+// };
+// const cityData = {
+//   Kerala:['Kochi','Kozhikode','Thrissur','Kollam'],
+//   Tamilnadu:['Chennai','Coimbatore','Madurai','Tiruchirappalli'],
+//   Andhara:['Hyderabad','Visakhapatnam','Vijayawada','Warangal'],
+//   Karnataka:['Bengaluru','Mysuru','Hubballi','Davangere'],
+// };
 
 function App() {
-  // useEffect(()=>{
-  //   apiServices.userGetList().then ((data)=>
-  //     {
-  //       console.log(data, 'fffccfd');
-  //     }
-  // )
-  // },[]
-  // )
+  const countriesList = [
+    {
+      id: 1,
+      name: "India"
+    },
+    {
+      id: 2,
+      name: "United States"
+    },
+    {
+      id: 3,
+      name: "United Kingdom"
+    },
+    {
+      id: 4,
+      name: "Canada"
+    }
+  ];
 
+  const statesList = [
+    {
+      id: 1,
+      name: "Maharashtra",
+      country_id: 1
+    },
+    {
+      id: 2,
+      name: "Karnataka",
+      country_id: 1
+    },
+    {
+      id: 3,
+      name: "New York",
+      country_id: 2
+    }
+  ];
+
+  const citiesLits = [
+    {
+      id: 1,
+      name: "Mumbai",
+      state_id: 1
+    },
+    {
+      id: 2,
+      name: "Bangalore",
+      state_id: 2
+    },
+    {
+      id: 3,
+      name: "New York City",
+      state_id: 3
+    }
+  ];
+
+  const [countries, setCountries] = useState();
+  const [states, setStates] = useState();
+  const [cities, setCities] = useState();
 
   // css for form layout
   const layout = { labelCol: { span: 5,}, wrapperCol: { span: 15,}, };
@@ -30,30 +89,39 @@ function App() {
   };
 
   //Country,state and city
-  const [cities, setCities] = useState( 
-    cityData
-    [stateData[0]]
-  );
-  const [secondCity, setSecondCity] = useState(
-    cityData
-    [stateData[0]]
-    [0]);
-  const handleProvinceChange = (value) => {
-    setCities(cityData[value]);
-    setSecondCity(cityData[value][0]);
+
+  const handleCountryChange = (value)=>{
+    console.log(value, "countryData");
+    setCountries(value)
+    setStates("")
+    setCities("")
   };
-  const onSecondCityChange = (value) => {
-    setSecondCity(value);
+  const onStateChange = (value) => {
+    setStates(value);
+    setCities("")
   };
 
+  const onSelectCities =(value) =>{
+      setCities(value)
+  }
+
+  const stateFilterList=statesList.filter((state) => (
+      state.country_id==countries
+  ));
+      // console.log(stateFilterList, "stateFilterList");
+  
+  const cityFilterList=citiesLits.filter((state) => (
+        state.state_id==states
+    ));
+        // console.log(cityFilterList, "cityFilterList");
+
   const onFinish = (values) => {
-    //
     console.log(values);
   };
 
   return (
-
     <div className="App">
+      
 {/* form */}
       <Form {...layout} name="nest-messages" onFinish={onFinish}
     style={{ maxWidth: 600, }}
@@ -79,29 +147,37 @@ function App() {
       <InputNumber />
     </Form.Item>
 
-    <Form.Item label="Country">
-      <Select/>
-    </Form.Item>
-
-    <Form.Item label="State" >
+    <Form.Item label="Country" >
     <Select
-        defaultValue={stateData[0]}
-        onChange={handleProvinceChange}
-        options={stateData.map((province) => ({
-          label: province,
-          value: province,
+        defaultValue=""
+        onChange={handleCountryChange}
+        options={countriesList.map((country) => ({
+          label: country.name,
+          value: country.id,
         }))}
       />
     </Form.Item>
 
-    <Form.Item label="City" >
+    <Form.Item label="State" >
     <Select
-        value={secondCity}
-        onChange={onSecondCityChange}
-        options={cities.map((city) => ({
-          label: city,
-          value: city,
+        value={states}
+        onChange={onStateChange}
+        options={stateFilterList.map((state) => ({
+          label: state.name,
+          value: state.id,
+
         }))}
+      />
+    </Form.Item>
+
+    <Form.Item label="Cities">
+      <Select
+        value={cities}
+        onChange={onSelectCities}
+        options={cityFilterList.map((city) => ({
+          label: city.name,
+          value: city.id,
+          }))}      
       />
     </Form.Item>
 
